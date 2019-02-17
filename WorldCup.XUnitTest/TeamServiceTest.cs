ee3e34ed648648f938d5a6c52ad7ca4eb449bc21
Alexander <Alexander@DESKTOP-8RM83T5>
2019-02-16 22:04:12 -0200

    Added unit test of TeamService class

--- WorldCup.XUnitTest/TeamServiceTest.cs
+++ WorldCup.XUnitTest/TeamServiceTest.cs
@@ -1,54 +1,56 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WorldCup.Domain.Entities;
+using WorldCup.DomainService.Interfaces;
+using WorldCup.DomainService.Services;
 using WorldCup.Infra.InMemory.Context;
 using WorldCup.Infra.InMemory.Repositories;
 using Xunit;
-using WorldCup.Domain.Entities;
-using System.Linq;
 using FluentAssertions;
-using WorldCup.Domain.Interfaces.Repositories;
+using System.Linq;
 
 namespace WorldCup.XUnitTest
 {
     /// <summary>
     /// Classe de teste que garante o funcionamento
-    /// das operações realizadas pela classe TeamRepository.
+    /// das operações realizadas pela classe TeamService.
     /// @Autor: Alexander Silva
     /// </summary>
-    public class TeamRepositoryTest
+    public class TeamServiceTest
     {
         [Fact]
         public void TestAddTeam()
         {
             /* ================== Montando Cenario =================== */
-            ITeamRepository teamRepository = GetInMemoryTeamRepository();
-            var brasil = new Team(1, "Brasil", "BRA");
             
+            ITeamService teamService = new TeamService(GetInMemoryTeamRepository()) ;
+            var brasil = new Team(1, "Brasil", "BRA");
+
             /* ================== Execucao =================== */
-            var teamSaved = teamRepository.Add(brasil);
+            var teamSaved = teamService.Add(brasil);
 
             /* ================== Verificacao =================== */
 
             // Testando com Assert
-            //Assert.Single(teamRepository.GetAll().ToList());
+            //Assert.Single(teamService.GetAll().ToList());
             //Assert.Equal(1, teamSaved.TeamId);
             //Assert.Equal("Brasil", teamSaved.Name);
             //Assert.Equal("BRA", teamSaved.Flag);
 
             // Testando com FluentAssertions
-            teamRepository.Should().NotBeNull(teamRepository.GetAll().ToList().Count.ToString(), 
+            teamService.Should().NotBeNull(teamService.GetAll().ToList().Count.ToString(),
                 $"O objeto esperado não corresponde com ao objeto obtido" +
-                $" ({teamRepository.GetAll().ToList().Count.ToString()})");
+                $" ({teamService.GetAll().ToList().Count.ToString()})");
 
-            teamSaved.TeamId.Should().Be(1, 
+            teamSaved.TeamId.Should().Be(1,
                 $"O Id do objeto esperado não corresponde com o Id do objeto obtido {teamSaved.TeamId}");
-            teamSaved.Name.Should().Be("Brasil", 
+            teamSaved.Name.Should().Be("Brasil",
                 $"O nome do time esperado não corresponde com o nome obtido {teamSaved.Name}");
-            teamSaved.Flag.Should().Be("BRA", 
+            teamSaved.Flag.Should().Be("BRA",
                 $"A bandeira não corresponde coma a obtida {teamSaved.Flag}");
         }
 
         private TeamRepository GetInMemoryTeamRepository()
-        {            
+        {
             DbContextOptions<WorldCupContext> options;
             var builder = new DbContextOptionsBuilder<WorldCupContext>();
             options = builder.UseInMemoryDatabase(databaseName: "ConnectionTest").Options;
